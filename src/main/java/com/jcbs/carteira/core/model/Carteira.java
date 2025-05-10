90086a4a8d2757aa955b5b44c39db3a7f9b5129b
julio barros <juliocbarros2@gmail.com.br>
2025-05-10 12:00:08 -0300

    MVP-CARTEIRA - fix: retirando agentes externos do core

--- src/main/java/com/jcbs/carteira/core/model/Carteira.java
+++ src/main/java/com/jcbs/carteira/core/model/Carteira.java
@@ -2,32 +2,11 @@ package com.jcbs.carteira.core.model;
 
 import java.math.BigDecimal;
 
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "carteira")
 public class Carteira {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "usuario_id", nullable = false)
     private Usuario usuario;
-
-    @Column(nullable = false, precision = 19, scale = 4)
     private BigDecimal saldo;
-
-    @Column(name = "limite_diario", precision = 19, scale = 4)
     private BigDecimal limiteDiario;
 
     public Carteira() {
