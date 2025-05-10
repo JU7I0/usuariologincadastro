5df79e0501aca9a2da43dd2343f7375cfe8eb612
julio barros <juliocbarros2@gmail.com.br>
2025-05-10 12:00:57 -0300

    MVP-CARTEIRA - feat: Criando representação das entidades

---
+++ src/main/java/com/jcbs/carteira/infrastructure/model/Carteira.java
@@ -0,0 +1,74 @@
+package com.jcbs.carteira.infrastructure.model;
+
+import java.math.BigDecimal;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "carteira")
+public class Carteira {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @Column(nullable = false, precision = 19, scale = 4)
+    private BigDecimal saldo;
+
+    @Column(name = "limite_diario", precision = 19, scale = 4)
+    private BigDecimal limiteDiario;
+
+    public Carteira() {
+    }
+
+    public Carteira(Long id, Usuario usuario, BigDecimal saldo, BigDecimal limiteDiario) {
+        this.id = id;
+        this.usuario = usuario;
+        this.saldo = saldo;
+        this.limiteDiario = limiteDiario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public BigDecimal getLimiteDiario() {
+        return limiteDiario;
+    }
+
+    public void setLimiteDiario(BigDecimal limiteDiario) {
+        this.limiteDiario = limiteDiario;
+    }
+}
---
+++ src/main/java/com/jcbs/carteira/infrastructure/model/Transacao.java
@@ -0,0 +1,103 @@
+package com.jcbs.carteira.infrastructure.model;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transacao")
+public class Transacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String tipo;
+
+    @Column(nullable = false, precision = 19, scale = 4)
+    private BigDecimal valor;
+
+    @Column(name = "origem_id")
+    private Long origemId;
+
+    @Column(name = "destino_id")
+    private Long destinoId;
+
+    @Column(length = 20, nullable = false)
+    private String status;
+
+    @Column(name = "data_criacao", nullable = false)
+    private LocalDateTime dataCriacao;
+
+
+    public Transacao() {
+    }
+
+    public Transacao(Long id, String tipo, BigDecimal valor, Long origemId, Long destinoId, String status, LocalDateTime dataCriacao) {
+        this.id = id;
+        this.tipo = tipo;
+        this.valor = valor;
+        this.origemId = origemId;
+        this.destinoId = destinoId;
+        this.status = status;
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public Long getOrigemId() {
+        return origemId;
+    }
+
+    public void setOrigemId(Long origemId) {
+        this.origemId = origemId;
+    }
+
+    public Long getDestinoId() {
+        return destinoId;
+    }
+
+    public void setDestinoId(Long destinoId) {
+        this.destinoId = destinoId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+}
