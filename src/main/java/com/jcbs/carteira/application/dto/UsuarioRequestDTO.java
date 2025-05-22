6b2b4dbb2ed2c400cd8d05704f2bbdfb685d7d62
julio barros <juliocbarros2@gmail.com.br>
2025-05-22 20:22:21 -0300

    MVP-CARTEIRA - feat: ajustando dtos para class

--- src/main/java/com/jcbs/carteira/application/dto/UsuarioRequestDTO.java
+++ src/main/java/com/jcbs/carteira/application/dto/UsuarioRequestDTO.java
@@ -1,9 +1,19 @@
 package com.jcbs.carteira.application.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString @EqualsAndHashCode
+public class UsuarioRequestDTO {
 
-public record UsuarioRequestDTO(
-    String cpf,
-    String email,
-    String nome,
-    String senha
-) {}
+    private String cpf;
+    private String email;
+    private String nome;
+    private String senha;
+}
